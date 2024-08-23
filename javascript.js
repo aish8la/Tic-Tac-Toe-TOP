ce643d0595535a85e5cc1a8186f1c0216a5730cb
aish8la <64914670+aish8la@users.noreply.github.com>
2024-08-24 03:02:17 +0500

    add Player Module

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 //Game Board Module
 const gameBoard = (function() {
     
-    const board = [0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0 ];
+    const board = [];
     
     function initBoard() {
         for (let i = 0; i < 9; i++) {
@@ -36,10 +34,39 @@ const gameBoard = (function() {
 
 })();
 
+//Player Module
+const playerModule = (function() {
+    const players = {
+        player1: {
+            name: 'Player 1',
+            mark: 'X',
+        },
+
+        player2: {
+            name: 'Player 2',
+            mark: 'O',
+        },
+    };
+
+    const setName = function(playerKey, newName) {
+        players[playerKey].name = newName;
+        console.log(players);
+    };
+
+    const getPlayer = function(playerKey) {
+        console.log(players[playerKey]);
+        return { ...players[playerKey]};
+    };
+
+    return {
+        setName,
+        getPlayer,
+    };
+})();
+
+
+//Initializer
 addEventListener('DOMContentLoaded', () => {
     gameBoard.initBoard();
-})
-
-gameBoard.addMark(-1, 1);
-
+});
 
